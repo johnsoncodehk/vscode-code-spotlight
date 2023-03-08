dfffbbe72a7dbfba93cbd6d339e132b86acadce7
johnsoncodehk <johnsoncodehk@gmail.com>
2023-03-09 07:43:38 +0800

    feat: enhance, weaken

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,78 @@
 import * as vscode from 'vscode';
 
-const decorations = [
-	vscode.window.createTextEditorDecorationType({ opacity: '1' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.8' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.6' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.5' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.45' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.4' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.35' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.3' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.25' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.2' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.15' }),
-	vscode.window.createTextEditorDecorationType({ opacity: '0.1' }),
-];
-const decorationRanges = decorations.map(() => [] as vscode.Range[]);
-
 let subscription: vscode.Disposable | undefined;
+let pows = [
+	3 * (1.5 ** -4),
+	3 * (1.5 ** -3),
+	3 * (1.5 ** -2),
+	3 * (1.5 ** -1),
+	3 * (1.5 ** 0),
+	3 * (1.5 ** 1),
+	3 * (1.5 ** 2),
+	3 * (1.5 ** 3),
+	3 * (1.5 ** 4),
+];
+let pow = pows[4];
+
+const decorations: vscode.TextEditorDecorationType[] = [];
+const decorationRanges: vscode.Range[][] = [];
 
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.commands.registerCommand('toggleCodeSpotlight', () => {
+	vscode.commands.registerCommand('codeSpotlight.toggle', () => {
 		if (subscription) {
 			subscription.dispose();
 			subscription = undefined;
+			vscode.commands.executeCommand('setContext', 'codeSpotlight.enabled', false);
 		}
 		else {
 			subscription = vscode.window.onDidChangeTextEditorSelection(update);
+			vscode.commands.executeCommand('setContext', 'codeSpotlight.enabled', true);
 		}
 		update();
 	});
+
+	vscode.commands.registerCommand('codeSpotlight.enhance', () => {
+		if (pows.indexOf(pow) >= pows.length - 1) {
+			return;
+		}
+		pow = pows[Math.min(pows.length - 1, pows.indexOf(pow) + 1)];
+		updateDecorations();
+	});
+
+	vscode.commands.registerCommand('codeSpotlight.weaken', () => {
+		if (pows.indexOf(pow) <= 0) {
+			return;
+		}
+		pow = pows[Math.max(0, pows.indexOf(pow) - 1)];
+		updateDecorations();
+	});
+
+	updateDecorations();
+
+	function updateDecorations() {
+
+		vscode.commands.executeCommand('setContext', 'codeSpotlight.enhance.enabled', pow < pows[pows.length - 1]);
+		vscode.commands.executeCommand('setContext', 'codeSpotlight.weaken.enabled', pow > pows[0]);
+
+		const oldDecorations = [...decorations];
+		decorations.length = 0;
+		decorationRanges.length = 0;
+		for (let i = 0; i < 10; i++) {
+			const opacity = Math.max(0.1, Math.pow(0.9, i * pow));
+			decorations.push(vscode.window.createTextEditorDecorationType({ opacity: opacity.toString() }));
+			decorationRanges.push([]);
+			if (opacity <= 0.1) {
+				break;
+			}
+		}
+		update();
+		setTimeout(() => {
+			for (let i = 0; i < oldDecorations.length; i++) {
+				vscode.window.activeTextEditor?.setDecorations(oldDecorations[i], []);
+			}
+		}, 100);
+	}
 
 	async function update() {
 
@@ -58,14 +101,14 @@ export function activate(context: vscode.ExtensionContext) {
 				const remain = [...decorations];
 				const ranges = [...decorationRanges];
 				for (let i = 0; i < selectionRanges.length; i++) {
-					const range = selectionRanges[i];
-					const childRange = i >= 1 ? selectionRanges[i - 1] : undefined;
 					let decoration = remain.shift();
 					let decorationRange = ranges.shift();
 					if (!decoration || !decorationRange) {
 						decoration = decorations[decorations.length - 1];
 						decorationRange = decorationRanges[decorations.length - 1];
 					}
+					const range = selectionRanges[i];
+					const childRange = i >= 1 ? selectionRanges[i - 1] : undefined;
 					if (!childRange) {
 						decorationRange.push(new vscode.Range(range.startLineNumber - 1, range.startColumn - 1, range.endLineNumber - 1, range.endColumn - 1));
 					}
